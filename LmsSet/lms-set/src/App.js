8b90f12a09483390d08d0f113cd008256f9a30e9
井上涼介 <ryosuke.inoue0314@gmail.com>
2020-03-27 18:25:31 +0900

    createHeader

--- LmsSet/lms-set/src/App.js
+++ LmsSet/lms-set/src/App.js
@@ -2,7 +2,7 @@
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { createGenerateClassName, StylesProvider } from '@material-ui/styles'
 import React from 'react'
-import AppRouter from './components/AppRouter'
+import AppRouter from './pages/AppRouter'
 
 const generateClassName = createGenerateClassName({ productionPrefix: 'c' })
 
--- LmsSet/lms-set/src/pages/AppRouter.js
+++ LmsSet/lms-set/src/pages/AppRouter.js
@@ -1,14 +1,20 @@
-
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import AppContent from '../components/AppContent'
+import AppHeader from '../components/AppHeader'
 import Home from './Home'
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+      <>
+        <AppHeader />
+        <AppContent>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </AppContent>
+      </>
     </BrowserRouter>
   )
 }
